92d13f921a2600877a38b8f2dbd8250f2cae31d7
Reginald-kyalo <reginald.kyalo@gmail.com>
2024-08-12 01:18:20 +0300

    added module documentation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Implements Pascal’s triangle of n
+"""
 def pascal_triangle(n):
-    """
-    Returns a list of lists of integers
+    """Returns a list of lists of integers
     representing the Pascal’s triangle of n
     """
     if n <= 0:
